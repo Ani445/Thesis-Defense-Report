c33a2e0c110aaf29c3a71ec93125c4bc56f1be3c
abeshahsan <abeshahsan2002@gmail.com>
2025-09-07 08:35:04 +0600

    images

--- figures/val_labels/copying.bat
+++ figures/val_labels/copying.bat
@@ -2,8 +2,8 @@
 setlocal enabledelayedexpansion
 
 REM Set source and target directories
-set "SRC=C:\Users\abesh\Downloads\Weclip results and imges\Weclip results and imges\weclip_val_pseudolabels\content\WeCLIP\all_cams\new"
-set "DST=F:\UNI_STUFF\Thesis\Thesis-Defense-Report\figures\val_labels\weclip"
+set "SRC=H:\archive\VOC2012\JPEGImages"
+set "DST=F:\UNI_STUFF\Thesis\Thesis-Defense-Report\figures\originals"
 set "LIST=figures\val_cams\ours\t.txt"
 
 REM Make sure the target directory exists
